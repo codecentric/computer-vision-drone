26a3e84e1549c59ce2d7c3f036ecbfc71fe4c2d0
Oliver Moser <omoser@ombook.fritz.box>
2017-02-05 21:03:58 +0100

    added HUD test

--- src/pydrone/test/hud-test.py
+++ src/pydrone/test/hud-test.py
@@ -12,12 +12,16 @@ while video.isOpened():
 
     if frame_idx < 100:
         pydrone.hud.get_hud(frame, action="STARTING DRONE", idx=frame_idx)
+        cv2.imshow("video", frame)
+
     elif 100 < frame_idx < 1000:
-        pydrone.hud.get_hud(frame, action=None, idx=frame_idx)
-        pydrone.detectors.detect_person(frame)
+        if frame_idx % 2 == 0:
+            pydrone.hud.get_hud(frame, action=None, idx=frame_idx)
+            pydrone.detectors.detect_person(frame)
+            cv2.imshow("video", frame)
 
 
-    cv2.imshow("video", frame)
+    #cv2.imshow("video", frame)
     cv2.moveWindow("video", 10, 10)
 
     cv2.waitKey(1) & 0xff
