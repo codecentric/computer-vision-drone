ec925cc10fc6def0dca571c012bea2f8eca12283
Tobias Schaber <tobias.schaber01@metro.de>
2017-02-09 14:23:16 +0100

    clean up measurement results a bit

--- src/lab/ultrasonic/sensor-object-test/Sensor.js
+++ src/lab/ultrasonic/sensor-object-test/Sensor.js
@@ -19,13 +19,14 @@ function Sensor(pinTrigger, pinEcho, name) {
     this.pinTrigger = pinTrigger;
     this.pinEcho = pinEcho;
     this.name = name;
-    this.distance = 0;
+    this.distances = [0, 0, 0 ,0 ,0]; // fixed length
     this.internalSensor = usonic.createSensor(this.pinEcho, this.pinTrigger, 750, true);
 }
 
 /* refresh the drone. will be called in an interval */
 Sensor.prototype.refresh = function() {
-    this.distance = this.internalSensor();
+    this.distances.push(this.internalSensor());
+    this.distances.shift();
 }
 
 /* trigger the measurement background job */
@@ -35,7 +36,18 @@ Sensor.prototype.triggerStart = function() {
 }
 
 
+/* take the list of past measurements, remove the max and min value, and build
+ * an average, and round that to 2 digits
+ */
 Sensor.prototype.getDistance = function() {
+    return (
+        this.distances.reduce(
+            function(a,b) { return a+b}
+        )
+        - Math.max.apply(Math, this.distances)
+        - Math.min.apply(Math, this.distances)
+        ) / (this.distances.length-2)
+            .toFixed(2);
 
-    return this.distance.toFixed(2);
+    //return this.distance.toFixed(2);
 }
