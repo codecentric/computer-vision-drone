7136ee65f3e71871725d6b9f649e88522f768162
Tobias Schaber <tobias.schaber01@metro.de>
2017-02-16 09:38:44 +0100

    extended the buzzer for blink usage with leds

--- src/main/drone/sensors/Buzzer.js
+++ src/main/drone/sensors/Buzzer.js
@@ -1,9 +1,9 @@
 /**
  * @Author Tobias Schaber, codecentric AG
  *
- * This class is used to switch on or off an acoustic buzzer mounted on the drone.
+ * This class is used to switch on or off an acoustic buzzer or a LED mounted on the drone.
  *
- * Simply use the constructor to create a new buzzer and switch it on or off with the
+ * Simply use the constructor to create a new buzzer/LED and switch it on or off with the
  * switch() method.
  *
  * See https://github.com/Soarez/node-wiring-pi for details to the library
@@ -20,7 +20,7 @@ module.exports = Buzzer;
 
 /**
  * constructor to create a new buzzer.
- * @param pinOut the data pin on which the buzzer is connected to
+ * @param pinOut the data pin on which the buzzer/LED is connected to
  * @param name the name of this buzzer
  * @constructor
  */
@@ -37,7 +37,7 @@ Buzzer.OFF = 0;
 Buzzer.ON = 1;
 
 /**
- * switch the buzzer on or off.
+ * switch the buzzer or LED on or off.
  * @param mode takes Buzzer.ON to turn the buzzer on or Buzzer.OFF to switch the buzzer off.
  */
 Buzzer.prototype.switch = function(mode) {
@@ -46,5 +46,20 @@ Buzzer.prototype.switch = function(mode) {
     } else {
         wpi.digitalWrite(this.pinOut, wpi.LOW);
     }
+}
+
+
+/**
+ * let the buzzer or LED blink
+ * @param times how often should we blink?
+ * @param delay time in ms between two ON states
+ */
+Buzzer.prototype.blink = function(times, delay) {
+
+    for(var i=0; i<times; i++) {
+        setTimeout(this.switch.bind(this, Buzzer.ON), (i*delay));
+        setTimeout(this.switch.bind(this, Buzzer.OFF), (i*delay)+(delay/2));
+    }
+
 }
 
