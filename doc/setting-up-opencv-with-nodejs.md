ca69bbaa73e31b25d6bf5af42f083e640508cb87
Oliver Moser <oliver.moser@codecentric.de>
2017-02-17 11:39:00 +0100

    opencv test

--- doc/setting-up-opencv-with-nodejs.md
+++ doc/setting-up-opencv-with-nodejs.md
@@ -2,14 +2,17 @@
 
 ## Install OpenCV 
 
-Follow the basic tutorial from pyimagesearch:
+Follow the basic tutorial to install opencv binaries from pyimagesearch:
 
 * follow the steps in [install opencv](http://www.pyimagesearch.com/2016/10/24/ubuntu-16-04-how-to-install-opencv/)
 
 ## Further steps to integrate nodejs
 
 ``` 
-sudo apt-get install nodejs npm
+sudo apt-get install nodejs npm nodejs-legacy
 sudo apt-get install imagemagick 
-npm install --save opencv
+npm install opencv
 ```
+
+
+
--- src/lab/opencv/HelloOpenCV.js
+++ src/lab/opencv/HelloOpenCV.js
@@ -7,17 +7,29 @@ try {
     var camera = new cv.VideoCapture(0);
     var window = new cv.NamedWindow('Video', 0);
 
-     setInterval(function() {
-         camera.read(function(err, im) {
-             if (err) throw err;
-             console.log(im.size());
-             if (im.size()[0] > 0 && im.size()[1] > 0) {
-                 window.show(im);
-             }
-             window.blockingWaitKey(0, 50);
-         });
-     }, 20);
+    setInterval(function () {
 
+        camera.read(function (err, frame) {
+            if (err) throw err;
+
+            if (frame.size()[0] > 0 && frame.size()[1] > 0) {
+
+                frame.detectObject(cv.FACE_CASCADE, {}, function (err, faces) {
+                    if (err) throw err;
+                    if (!faces.length) return console.log("No Faces");
+                    console.log("found FACES!!!");
+
+                    for (var i = 0; i < faces.length; i++) {
+                        var face = faces[i];
+                        frame.rectangle([face.x, face.y], [face.x + face.width, face.y + face.height], [0, 255, 0], 2);
+
+                    }
+                    window.show(frame);
+                });
+                res = window.blockingWaitKey(0, 50);
+            }
+        })
+    }, 20);
 } catch (e){
     console.log("Couldn't start camera:", e)
 }
